f653ca2bbc22e170a3ccddbe64540e8c4fcf05f4
Juana Molina <juana2001molina@gmail.com>
2022-05-29 21:42:06 -0300

    primer comit

---
+++ ejercicios.cpp
@@ -0,0 +1,72 @@
+#include "ejercicios.h"
+#include "auxiliares.h"
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+/******++++**************************** EJERCICIO tiempoTotal ***********+++***********************/
+tiempo tiempoTotal(viaje v) {
+    tiempo t;
+    // codigo
+
+    return t;
+}
+
+/************++*********************** EJERCICIO distanciaTotal ************++*********************/
+distancia distanciaTotal(viaje v) {
+    distancia d;
+    // codigo
+
+    return d;
+}
+
+/*****************************+***** EJERCICIO excesoDeVelocidad **********************************/
+bool excesoDeVelocidad(viaje v) {
+    bool resp = false;
+    // codigo
+
+    return resp;
+}
+
+/************************************ EJERCICIO recorridoCubierto *******************************/
+vector<gps> recorridoNoCubierto(viaje v, recorrido r, distancia u) {
+    vector<gps> resp;
+    // codigo
+
+    return resp;
+}
+
+/***************************************** EJERCICIO flota ***************************************/
+int flota(vector<viaje> f, tiempo t0, tiempo tf) {
+    int resp;
+    // codigo
+
+    return resp;
+}
+
+/************************************** EJERCICIO construirGrilla *******************************/
+grilla construirGrilla(gps esq1, gps esq2, int n, int m) {
+    grilla resp = {};
+    // codigo
+
+    return resp;
+}
+
+/************************************* EJERCICIO cantidadDeSaltos ******************************/
+int cantidadDeSaltos(grilla g, viaje v) {
+    int resp;
+    // codigo
+
+    return resp;
+}
+
+
+/************************************* EJERCICIO corregirViaje ******************************/
+void corregirViaje(viaje& v, vector<tiempo> errores){
+    // codig
+
+    return;
+}
